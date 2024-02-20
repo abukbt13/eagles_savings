af38d0774b2e297d81c4223dd95d8778ea1970d9
Freemano <freemano@MACs-MacBook-Pro.local>
2024-02-20 07:17:01 +0300

    Remove env.php from repository

--- env.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-global $from;
-$from='SHARA';
-global $api_token;
-$api_token='BjBz8xAii6Tb7c8C4xhTBrUJkl91cSYD3Kt3n3AtQy56LtBczsVE5b3IFORUIqMVrhnjMXfRM2XdYDbgfcA2FQ';
-global $CURLOPT_URL;
-$CURLOPT_URL= 'https://app.sharasms.co.ke/api/sms/send';
